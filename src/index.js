42af94ec7f76eca91bb5dfcbd4b55f6000482af8
Steve Fan <29133953+stevefan1999@users.noreply.github.com>
2018-08-24 17:26:10 +0800

    Fix multi-indent pug with a very dumb hack

--- src/index.js
+++ src/index.js
@@ -1,34 +1,23 @@
-var render = require('pug').render
-var transform = require('babel-core').transform
+var render = require("pug").render;
+var transform = require("babel-core").transform;
 
-module.exports = function () {
+module.exports = function({ types: t }) {
   return {
     visitor: {
-      TaggedTemplateExpression: function (path, state) {
-        if (path.node.tag.name === 'pug') {
-          var raw = path.node.quasi.quasis[0].value.raw
-          var splitedRaw = raw
-            .split('\n')
-            .filter(function(str) {
-              return str !== '' && str.match(/^\s*$/g) === null
-            })
-
-          var rootIndent = /^\s*/.exec(splitedRaw[0])[0]
-          var fixedRaw = splitedRaw.map(function (raw) {
-            var spaceRegExp = new RegExp(`^${rootIndent}`)
-            return raw.replace(spaceRegExp, '')
-          }).join('\n')
-
-          var html = render(fixedRaw)
-              .replace(/"\{/g, '{').replace(/\}"/g, '}').replace(/\};"/g, '}')
-              .replace(/class="/g, 'className="').replace(/for="/g, 'htmlFor="')
-              .replace(/\\\`/g, '`')
-
-          html = '`' + html + '`'
-          var ast = transform(html).ast
-          path.replaceWithMultiple(ast.program.body)
+      TaggedTemplateExpression(path, state) {
+        if (path.node.tag.name === "pug") {
+          path.replaceWith(
+            t.expressionStatement(
+              t.stringLiteral(
+                render(
+                  "<!>\n" +
+                    path.node.quasi.quasis[0].value.raw.replace(/\s+$/, "")
+                ).replace("<!>", "")
+              )
+            )
+          );
         }
       }
     }
-  }
-}
+  };
+};
---
+++ test/spec/complex.4.js
@@ -0,0 +1,23 @@
+var spec = {
+  template: pug`.product-item
+    .product-title {{ product.title }}
+    .product-details
+      .inventory In Stock: {{ product.inventory }}
+      button.add-button(:disabled='!product.inventory', @click='addToCart(product)') {{ product.inventory > 0 ? "Add to cart" : "Out Of Stock" }}   
+  
+  `,
+  expected: `
+    <div class="product-item">
+      <div class="product-title">{{ product.title }}</div>
+      <div class="product-details">
+        <div class="inventory">In Stock: {{ product.inventory }}</div>
+        <button
+          class="add-button"
+          :disabled="!product.inventory"
+          @click="addToCart(product)">
+          {{ product.inventory > 0 ? "Add to cart" : "Out Of Stock" }}
+        </button>
+      </div>
+    </div>
+  `
+};
